18e9f133307b94c0237069cecd535c53a44d8a01
Ranie Santos <ranietots32@gmail.com>
2016-06-01 03:50:31 +0800

    codecourse tutorial part 6 done

---
+++ app/Controllers/ProductController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Cart\Controllers;
+
+use Slim\Router;
+use Slim\Views\Twig;
+use Cart\Models\Product;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class ProductController{
+
+  protected $view;
+
+  public function __construct(Twig $view){
+    $this->view = $view;
+  }
+
+	public function get($slug, Request $request, Response $response, Twig $view, Router $router, Product $product){
+		$product = $product->where('slug', $slug)->first();
+
+    if (!$product) {
+      return $response->withRedirect($router->pathFor('home'));
+    }
+
+		return $this->view->render($response, 'products/product.twig', [
+			'product' => $product
+		]);
+	}
+
+}
