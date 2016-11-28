b89c5c301355d3e8e8f97a3c2f50d0fa791d9040
Ranie Santos <ranietots32@gmail.com>
2016-11-28 08:15:03 +0800

    created order form view

---
+++ app/Controllers/OrderController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Cart\Controllers;
+
+use Slim\Router;
+use Slim\Views\Twig;
+use Cart\Basket\Basket;
+use Cart\Models\Product;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class OrderController{
+
+	protected $basket;
+
+	protected $router;
+
+	public function __construct(Basket $basket, Router $router){
+		$this->basket = $basket;
+		$this->router = $router;
+	}
+
+	public function index(Request $request, Response $response, Twig $view){
+		$this->basket->refresh();
+
+		if(!$this->basket->subTotal()){
+			return $response->withRedirect($this->router->pathFor('cart.index'));
+		}
+		
+		return $view->render($response, 'order/index.twig');
+	}
+
+	public function create(Request $request, Response $response){
+		$this->basket->refresh();
+
+		if(!$this->basket->subTotal()){
+			return $response->withRedirect($this->router->pathFor('cart.index'));
+		}
+
+		
+	}
+
+}
